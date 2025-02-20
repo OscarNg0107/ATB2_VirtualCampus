66e1f6013904586a59c9d1485d417b1f5652cab9
OscarNg <sze4.ng@live.uwe.ac.uk>
2025-02-20 12:51:35 +0000

    Rotation Alignment Debug#1

--- Assets/Scripts/RotationAlignment.cs
+++ Assets/Scripts/RotationAlignment.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class RotationAlignment : MonoBehaviour
 {
+    [SerializeField] private TMPro.TextMeshPro m_TextMeshPro;
+
     // Update is called once per frame
     void Update()
     {
         SetSelfRotation();
+
     }
 
     private void SetSelfRotation() 
@@ -15,5 +19,6 @@ public class RotationAlignment : MonoBehaviour
         Vector3 rot = this.transform.eulerAngles;
         rot.z = -Input.compass.magneticHeading;
         this.transform.eulerAngles = rot;
+        m_TextMeshPro.text = Input.compass.magneticHeading.ToString();
     }
 }
