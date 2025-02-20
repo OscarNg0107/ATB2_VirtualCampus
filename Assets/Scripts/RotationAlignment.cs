4be4c8e9f83cd1a3f87455585f216a6ec7cf4438
OscarNg <sze4.ng@live.uwe.ac.uk>
2025-02-20 11:51:53 +0000

    Added rotation alignment

---
+++ Assets/Scripts/RotationAlignment.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationAlignment : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.eulerAngles = new Vector3(
+            gameObject.transform.eulerAngles.x,
+            AlignWithWorldNorth(),
+            gameObject.transform.eulerAngles.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public float AlignWithWorldNorth()
+    {
+        // Get the device's current compass heading relative to true north
+        float compassHeading = Input.compass.trueHeading;
+
+        // Calculate the rotation needed to align the AR scene with the desired north
+        //float trueRotation = (compassHeading - _settings.RoomNorthOffset + 360) % 360;
+        float trueRotation = compassHeading;
+
+        return trueRotation;
+    }
+}
